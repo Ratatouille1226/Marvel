192d5be34c2ef6cb022707e05e592c78c9bbf2e3
Andrey <kotlyar.andrey03@mail.ru>
2024-08-23 14:36:48 +0300

    Работа с рефами

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -2,6 +2,7 @@ import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 
+import PropTypes from 'prop-types';
 import { Component } from "react";
 
 import './charList.scss';
@@ -65,18 +66,35 @@ class CharList extends Component {
         })
     }
 
+    //РАБОТА С РЕФАМИ
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
     //Создадим метод для оптимизации, чтобы не помещать такую конструкцию в render
     rednderItems(arr) {
-       const items = arr.map((item) => {
+       const items = arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
             }
 
             return (
-                <li className="char__item" 
+                <li className="char__item"
+                ref={this.setRef} 
                 key={item.id}
-                onClick={() => this.props.onCharSelected(item.id)}
+                onClick={() => {
+                    this.props.onCharSelected(item.id);
+                    this.focusOnItem(i);
+                }}
                 >
                     <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
@@ -116,6 +134,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
